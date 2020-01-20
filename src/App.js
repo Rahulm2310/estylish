629b4a3c1755a8f270f958e8f37c72102f9ac4e3
Your Name <you@example.com>
2020-01-20 21:48:11 +0530

    updating our app with shop page,collection preview component & collection item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import Homepage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -13,8 +14,8 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route exact path='/shop/hats' component={HatsPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
