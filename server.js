85d6e5d44ada32d8d8cfe0a3d2443c9348e654db
Your Name <you@example.com>
2020-02-07 19:42:43 +0530

    added changes to server.js

--- server.js
+++ server.js
@@ -19,12 +19,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
 app.listen(port, error => {
-  if (error) throw error;
   console.log('Server running on port ' + port);
 });
 
