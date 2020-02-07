85c845278d4d1369f71d15983ff87a8c9b8ae5eb
Your Name <you@example.com>
2020-02-07 19:36:42 +0530

    modified server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
